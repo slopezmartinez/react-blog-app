600ce49bd04ce10b4b112a01ab2880ee4d5fe3c7
Trofi <145512110+mtrofibun@users.noreply.github.com>
2026-04-16 18:31:28 -0400

    Update accounts.jsx

--- my-react-app/src/accounts.jsx
+++ my-react-app/src/accounts.jsx
@@ -1,18 +1,15 @@
 const accounts =[
     {
-    id: 1,
     username: "Johnsmith125",
     email : "johnsmith@gmail.com",
     password: "abc1234",
     },
     {
-    id: 2,
     username: "MileySue56",
     email : "Mileysue@gmail.com",
     password: "abc1234",
     },
     {
-    id: 3,
     username: "SarahJenny",
     email : "Sarahjenny@gmail.com",
     password: "abc1234",
